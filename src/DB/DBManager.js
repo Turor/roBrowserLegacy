5539dcecf1deec3f98e3d2d77790e07cd75d5caf
Turor <Turor@users.noreply.github.com>
2026-08-30 21:02:17 -0500

    Load Turoran skilldescript and skillinfolist Lua

--- src/DB/DBManager.js
+++ src/DB/DBManager.js
@@ -481,14 +481,14 @@ class DB {
 				}
 				// Load description - skillid.lub is re-executed harmlessly (Lua just repopulates globals)
 				loadLuaTable(
-					[DB.LUA_PATH + 'skillinfoz/skillid.lub', DB.LUA_PATH + 'skillinfoz/skilldescript.lub'],
+					[DB.LUA_PATH + 'skillinfoz/skillid.lub', DB.LUA_PATH + 'skillinfoz/skilldescript.turoran.lub'],
 					'SKILL_DESCRIPT',
 					_json => {
 						SkillDescription = _json;
 					},
 					() => {
 						// Calls after skillids and descs been populated
-						loadSkillInfoList(DB.LUA_PATH + 'skillinfoz/skillinfolist.lub', null, () => {
+						loadSkillInfoList(DB.LUA_PATH + 'skillinfoz/skillinfolist.turoran.lub', null, () => {
 							loadSkillTreeView(DB.LUA_PATH + 'skillinfoz/skilltreeview.snbow.lub', null, () => {
 								// Load ez2streffect, PACKETVER unknown when the while has been added, tied to default PACKETVER of rathena for 4th job
 								if (PACKETVER.value >= 20211103) {
